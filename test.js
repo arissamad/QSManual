f9162e99e356c2f5f2712cf05b4c1901a6b5e081
Azreen Latiff <azreen@quickschools.com>
2012-09-05 23:08:01 -0400

    Making a node server.

---
+++ test.js
@@ -0,0 +1,2 @@
+console.log("Hey");
+console.log("Try 2");
---
+++ web.js
@@ -0,0 +1,14 @@
+console.log("Here");
+
+// Webserver v1
+var express = require('express');
+var app = express.createServer(express.logger()); // Can put in express.logger("tiny") or express.logger() if you want debugging
+
+// Static files in public
+app.use(express.static(__dirname + "/public"));
+
+// Routes all set. Start the server!
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Server started. Listening on " + port);
+});
