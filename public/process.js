c847d346c9d4f94601389312cbc5733179626eec
Azreen Latiff <azreen@quickschools.com>
2013-03-25 18:50:34 -0400

    Fernando: Lazy loading chapters

--- public/process.js
+++ public/process.js
@@ -4,34 +4,12 @@ if(!window.console.log) window.console.log=function(){};
 var chapterData = [];
 var pauseHashChangeDetection = false;
 
-function loadedPage() {
-	console.log("Loading chapters " + chapters.length);
-
-	var numLoaded = 0;
-	for(var i=0; i<chapters.length; i++) {
-		var chapter = chapters[i];
-
-		loadHtml(chapter, function(chapter, i) {
-			return function(data) {
-                numLoaded++;
-                chapterData[i] = data;
-                
-				console.log("Loaded chapter:", chapter, numLoaded + "/" + chapters.length);
-				
-				if(numLoaded == chapters.length) {
-					console.log("Loaded all html files.");
-					renderChapters();
-				}
-			}
-		}(chapter, i));
-	}
-}
-
 var h1Template;
 var h2Template;
 
-function renderChapters() {
+function loadedPage() {
     var toc1 = $(".toc1");
+    toc1.find(".accordionContent").hide();
     var tocParent = toc1.parent();
     
     h1Template = $("h1").first();
@@ -42,90 +20,28 @@ function renderChapters() {
     var contentDiv = $("#content-div");
     contentDiv.empty();
     
-    var srcAlt = "data-source";
-    //assumes div.center|left
-	for(var i=0; i<chapterData.length; i++) {
- 
-        var chapterFile = $(chapterData[i].replace(new RegExp("(<div\\s+class=\"(?:center|left)\"\\s*>\\s*)(<img[^>]*src *= *[\"']?)([^\"']*)","g"),"$1<img "+srcAlt+"=\"$3"));
-        console.log("DATA: ",chapterData[i].replace(new RegExp("(<div\\s+class=\"(?:center|left)\"\\s*>\\s*)(<img[^>]*src *= *[\"']?)([^\"']*)","g"),"$1<img "+srcAlt+"=\"$3"));
-        //var chapterFile = $(chapterData[i]);
-        
-        console.log("WHAT?",chapterFile);
-        var h1Id = chapterFile.find("h1").attr("id");
-        if(h1Id == null || h1Id == "") {
-            h1Id = "chapter-" + (i+1);
-        }
-        
-        
-		var chapterTitle = chapterFile.find("h1").text();
-		
-        var newToc = toc1.clone();
-        
-        newToc.find(".accordionButton>a").attr("href", "#" + h1Id);
-        newToc.find(".chap-text").text(chapterTitle);
-        newToc.find(".chap-num").text(getChapNum(i));
-        
-        console.log("newToc", newToc);
-        
-        tocParent.append(newToc);
-        
-        
-        var chapterContent = chapterFile.filter(".content");
-        
-        //resize images, assumes div.center, and div.left
-        var chapterImages = chapterContent.find("div.center > img, div.left img");
-        
-        chapterImages.filter(function(index){
-            return Number($(this).attr('width')) > 690;            
-        }).height(function(index,oldHeight){
-            var oldWidth = Number($(this).attr('width'));
-            var oldHeight = Number($(this).attr('height'));
-            return oldHeight*690/oldWidth;
-        }).width(function(index,oldWidth){
-            return 690;
-        });
-        
-        chapterImages.replaceWith(function(){
-            //console.log("ImageReplace", this, arguments);
-            var img = $(this);
-            
-            var width = img.width() || Number(img.attr("width"));
-            var height = img.height() || Number(img.attr("height"));
-            
-            var lazyDiv = $("<div></div>").addClass("lazy-image").height(height).width(width);
-            
-            //prevent loading of image
-            img.attr(srcAlt,img.attr("src"));
-            img.removeAttr("src");
-            
-            lazyDiv.data("img",img);
-            
-            lazyDiv.one("inview.QSManual", function(){
-                console.log("REPLACE IMAGE",$(this),lazyDiv.data("img"));
-                var img = lazyDiv.data("img");
-                img.attr("src",img.attr(srcAlt));
-                img.removeAttr(srcAlt);
-                lazyDiv.replaceWith(img);
-                lazyDiv.remove();
-            })
-            
-            return lazyDiv;
-        });
-        
-        console.log("chapterContent",chapterContent);
-        
-        contentDiv.append("<div class='anchor' id='" + h1Id + "'></div>");
-        
-        var section = $("<section></section>");
-        section.append(chapterContent);
-        
-        
-		contentDiv.append(section);
-        
-        processHeaders(h1Id, chapterContent, i, newToc);
-	}
+    $(".initial-load").hide();
+    $(".post-load").show();
     
-    $(window).on("scroll", function() {
+    console.log("Loading chapters " + chapters.length);
+	var numLoaded = 0;
+	for(var i=0; i<chapters.length; i++) {
+		var chapter = chapters[i];
+
+        //create chapter stub
+        createChapterStub(toc1,tocParent,contentDiv,i);
+        
+		loadHtml(chapter, function(chapter, i) {
+			return function(data) {
+                numLoaded++;
+                chapterData[i] = data;
+                
+				console.log("Loaded chapter:", chapter, numLoaded + "/" + chapters.length);
+				
+                renderChapter(toc1,tocParent,contentDiv,chapterData[i],i);
+                
+				if(numLoaded == chapters.length) {
+					    $(window).on("scroll", function() {
         //console.log("Scroll", this,arguments);
         //chapter buckets
         $("section div.content").each(function(index,chapter) {
@@ -133,7 +49,7 @@ function renderChapters() {
             if(isElementVisible(chapter)){
                 $(chapter).find("div.lazy-image").each(function(index,lazyDiv){
                     //console.log("Scroll", index, lazyDiv);
-                    if(isElementVisible(lazyDiv)){
+                    if(isElementVisible(lazyDiv, 10)){
                         $(lazyDiv).trigger("inview.QSManual");
                     }    
                 });
@@ -142,7 +58,113 @@ function renderChapters() {
     });
     
     finalSteps();
+    //trigger scroll so images inview load;
+    $(window).trigger("scroll");
+    
+				}
+			}
+		}(chapter, i));
+	}
 }
+
+function createChapterStub(toc1,tocParent,contentDiv,order) {
+    var  h1Id = "chapter-" + (order+1);
+    contentDiv.append("<div class='anchor' id='" + h1Id + "'></div>");
+    var divLoading = $("<div>Loading</div>").addClass("loading-section");
+    var section = $("<section></section>").append(divLoading)
+    contentDiv.append(section);
+    
+    var newToc = toc1.clone();
+    newToc.find(".accordionButton>a").attr("href", "#" + h1Id);
+    newToc.find(".chap-text").text("Loading");
+    newToc.find(".chap-num").text(getChapNum(order));
+    console.log("newToc", newToc);
+
+    tocParent.append(newToc);
+};
+
+function renderChapters() {
+	for(var i=0; i<chapterData.length; i++) {
+        renderChapter(toc1,tocParent,contentDiv,chapterData[i],i);
+	}
+}
+
+function renderChapter(toc1,tocParent,contentDiv,chapterData,order) {
+    var srcAlt = "data-source";
+    //assumes div.center|left
+    
+    //prevent image loading by replacing img[src=];
+    var chapterData = chapterData.replace(new RegExp("(<div\\s+class=\"(?:center|left)\"\\s*>\\s*)(<img[^>]*src *= *[\"']?)([^\"']*)","g"),"$1<img "+srcAlt+"=\"$3");
+    
+    console.log("chapterData: ",chapterData);
+    var chapterFile = $(chapterData);
+    
+    var h1Id = chapterFile.find("h1").attr("id");
+    if(h1Id === null || h1Id === "") {
+        h1Id = "chapter-" + (order+1);
+    }
+    var h1IdAlt = "chapter-" + (order+1);
+
+
+    var chapterTitle = chapterFile.find("h1").text();
+
+    var newToc = tocParent.find(".toc1").has(".accordionButton>a[href=#"+h1IdAlt+"]");
+    newToc.find(".accordionButton>a").attr("href", "#" + h1Id);
+    newToc.find(".chap-text").text(chapterTitle);
+    newToc.find(".chap-num").text(getChapNum(order));
+
+    console.log("newToc", newToc);
+
+    var chapterContent = chapterFile.filter(".content");
+
+    //resize images, assumes div.center, and div.left
+    var chapterImages = chapterContent.find("div.center > img, div.left img");
+
+    chapterImages.filter(function(index){
+        return Number($(this).attr('width')) > 690;            
+    }).height(function(index,oldHeight){
+        var oldWidth = Number($(this).attr('width'));
+        var oldHeight = Number($(this).attr('height'));
+        return oldHeight*690/oldWidth;
+    }).width(function(index,oldWidth){
+        return 690;
+    });
+
+    chapterImages.replaceWith(function(){
+        //console.log("ImageReplace", this, arguments);
+        var img = $(this);
+        
+        var width = img.width() || Number(img.attr("width"));
+        var height = img.height() || Number(img.attr("height"));
+        
+        var lazyDiv = $("<div></div>").addClass("lazy-image").height(height).width(width);
+        
+        //prevent loading of image
+        img.attr(srcAlt,img.attr("src"));
+        img.removeAttr("src");
+        
+        lazyDiv.data("img",img);
+        
+        lazyDiv.one("inview.QSManual", function(){
+            console.log("REPLACE IMAGE",$(this),lazyDiv.data("img"));
+            var img = lazyDiv.data("img");
+            img.attr("src",img.attr(srcAlt));
+            img.removeAttr(srcAlt);
+            lazyDiv.replaceWith(img);
+            lazyDiv.remove();
+        })
+        
+        return lazyDiv;
+    });
+
+    console.log("chapterContent",chapterContent);
+    var divAnchor = contentDiv.find("div.anchor#"+h1IdAlt);
+    divAnchor.attr("id",h1Id);
+    divAnchor.next("section").append(chapterContent).find("div.loading-section").remove();
+    
+    processHeaders(h1Id, chapterContent, order, newToc);
+
+};
 
 function processHeaders(h1Id, section, i, newToc) {
 	var header = section.find("h1");
@@ -170,7 +192,7 @@ function processHeaders(h1Id, section, i, newToc) {
         var h2 = $(h2s[j]);
         var h2Text = h2.text();
         var h2Id = h2.attr("id");
-        if(h2Id == null || h2Id == "") {
+        if(h2Id === null || h2Id === "") {
             h2Id = "chapter-" + (i+1) + "-" + (j+1);
         }
         
@@ -187,34 +209,31 @@ function processHeaders(h1Id, section, i, newToc) {
         toc2Holder.append(newToc2);
     }
     
-    if(h2s.length == 0) {
+    if(h2s.length === 0) {
         toc2Holder.detach();
     }
 }
 
 function finalSteps() {
-    $(".initial-load").hide();
-    $(".post-load").show();
-    
     processAnchors();
     ready2();
     
     // Process initial hash
     var hash = window.location.hash;
-    if(hash != null && hash != "") {
+    console.log("LOCATION: HASH", hash);
+    if(hash != null && hash != "" && hash != "#") {
         console.log("Initial scrollto:", hash);
         var anchor = $("a[href=" + hash +"]").first()
         scrollToSection(anchor, false);
     }
     
-    $(window).hashchange(function(event) {
+    $(window).on("hashchange", function(event) {
         if(!pauseHashChangeDetection) {
             console.log("Triggering hash change:", location.hash);
             
             var anchor = $("a[href=" + location.hash +"]");
             console.log("Anchor is ", anchor);
-            
-            scrollToSection(anchor, false);
+            if(anchor.length > 0) scrollToSection(anchor, false);
         }
     });
     
@@ -287,7 +306,8 @@ function processAnchors() {
 }
 
 function scrollToSection($anchor, isFromClick) {
-    var target = $anchor[0].hash;
+    console.log("scrollToSection",$anchor);
+    var target = $anchor.get(0).hash || $anchor.attr("href").charAt(0) == '#' && $anchor.attr("href");
     var $target = $(target);
         
     var offset = $target.offset().top;
@@ -340,11 +360,13 @@ function turnOnSection(sectionId) {
     $("." + sectionId).show();
 }
 
-function isElementVisible(elem){
+// bufferHeight adds to the top and bottom of the element
+function isElementVisible(elem, bufferHeight){
+    var bufferHeight = Number(bufferHeight) || 0;
     var docViewTop = $(window).scrollTop();
     var docViewBottom = docViewTop + $(window).height();
 
     var elemTop = $(elem).offset().top;
     var elemBottom = elemTop + $(elem).height();
-    return elemBottom > docViewTop && docViewBottom > elemTop;
+    return elemBottom+bufferHeight > docViewTop && docViewBottom > elemTop-bufferHeight;
 }
